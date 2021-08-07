b5e4ce2e06985986ab2c0de99e7777bca755395e
zjbmfl <zjbmfl8899@163.com>
2021-08-07 22:20:32 +0800

    1.two push

---
+++ src/main/com/zjb/demo/Demo.java
@@ -0,0 +1,11 @@
+package com.zjb.demo;
+
+/**
+ * @author Administrator
+ * @date 2021/08/07 22:09
+ **/
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
