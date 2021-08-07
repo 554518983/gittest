3a3b960cd36bc0872b6193d8117ed4767d1b3d1c
zjbmfl <zjbmfl8899@163.com>
2021-08-07 22:41:22 +0800

    six commit

--- src/main/com/zjb/demo/Demo.java
+++ src/main/com/zjb/demo/Demo.java
@@ -8,5 +8,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hi");
+        System.out.println("hi1");
     }
 }
